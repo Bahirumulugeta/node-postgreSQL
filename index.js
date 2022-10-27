ae99c3c071930f6559b82039cb3626805b5b581a
bahiru <bahirumulugeta1@gmail.com>
2022-10-27 16:07:25 +0200

    connect node app with postgres by using node-postgres package

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("node-postgreSQL driver");
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  password: "pg@password",
+  host: "localhost",
+  port: 5432,
+  database: "kemer",
+});
+client
+  .connect()
+  .then(() => console.log("connected successfully"))
+  .then(()=>client.query('select * from users'))
+  .then(results=>console.table(results.rows))
+  .catch((e) => console.log(e))
+  .finally(() => client.end);
